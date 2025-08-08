398c1e62589d35fdc64787cb6d6809756737303d
infoKraxin <119085077+infoKraxin@users.noreply.github.com>
2025-08-08 03:17:40 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,48 +1,52 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'api.raspapixoficial.com',
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.supabase.co',
-        pathname: '/storage/v1/object/public/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'raspadolar.vercel.app',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        pathname: '/infoKraxin/raspadolar/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ik.imagekit.io',
-        pathname: '/azx3nlpdu/**',
-      },
-      // --- ADICIONE ESTE BLOCO ---
-      {
-        protocol: 'https',
-        hostname: 'raspagreen.cloud',
-        pathname: '/storage/**',
-      },
-      // --------------------------
-    ],
-  },
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.raspapixoficial.com',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raspadolar.vercel.app',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '/infoKraxin/raspadolar/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        pathname: '/azx3nlpdu/**',
+      },
+      // --- ADICIONE ESTE BLOCO ---
+      {
+        protocol: 'https',
+        hostname: 'raspagreen.cloud',
+        pathname: '/storage/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'rdddmzabvuyo9kjb.public.blob.vercel-storage.com',
+        pathname: '/**',
+      },
+      // --------------------------
+    ],
+  },
 };
 
 module.exports = nextConfig;
-
